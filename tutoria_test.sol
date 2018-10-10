0bc67b1ec78effe0d6a87dddea9dce602cbf8e56
Facundo <31260330+Facundopeloso@users.noreply.github.com>
2018-10-09 21:41:20 -0300

    Update tutoria_test.sol

--- tutoria_test.sol
+++ tutoria_test.sol
@@ -6,38 +6,28 @@ contract tutoriaTest {
    
     Tutoria tutoriaParaTest;
     
-    address profesor = _profesor;
-    string memory materia = msg.sender;
-    address alumno = _alumno;
-    
-    function debeCrearUnaTutoria () {
-        
-        tutoriaParaTest = new Tutoria(profesor, materia);
-        
-        Assert.equal(tutoriaParaTest.getProfesor(), profesor, "Deberia obtener el profesor de la tutoria");
-        Assert.equal(tutoriaParaTest.getMateria(), "Paradigma", "Deberia obtener la materia de la tutoria");
-        Assert.equal(tutoriaParaTest.getAlumno(), alumno, 'Deberia obtener el alumno de la tutoria');
-        
-        Assert.equal(tutoriaParaTest.esConfirmado(), 0, 'Deberia devolver que no esta confirmada una tutoria');
-
+function beforeAll () {
+        string memory materia = "Paradigma";
+        address IdProfesor = 0x14723a09acff6d2a60dcdf7aa4aff308fddc160c;
+        tutoriaParaTest = new Tutoria(materia, IdProfesor);
     }
-    
-    function debeConfirmarUnaTutoria () {
-        
+function checkDebeCrearUnaTutoria () public {
+        address alumno = msg.sender;
+        Assert.equal(tutoriaParaTest.getMateria(), "Paradigma", "Deberia obtener la materia de la tutoria");
+        Assert.equal(tutoriaParaTest.getIdProfesor(), 0x14723a09acff6d2a60dcdf7aa4aff308fddc160c, "Deberia obtener el profesor de la tutoria");
+        Assert.equal(tutoriaParaTest.getAlumno(), alumno, "Deberia obtener el alumno de la tutoria");
+        Assert.equal(tutoriaParaTest.esConfirmado(), 0, 'Deberia devolver que no esta confirmada una tutoria');
+        //bool isTrue = (tutoriaParaTest!=null);
+        //assert.true(isTrue);
+  }
+ function checkDebeConfirmarUnaTutoria () public {
         Assert.equal(tutoriaParaTest.confirmar(), 1, 'Deberia confirma una tutoria');
         
-        Assert.equal(tutoriaParaTest.esConfirmado(), 1, 'Deberia devolver que esta confirmada una tutoria');
-        
+        Assert.equal(tutoriaParaTest.estaConfirmado(), 1, 'Deberia devolver que esta confirmada una tutoria');
     }
-    
-    
-     function debeCancelarUnaTutoria () {
-        
+function checkDebeCancelarUnaTutoria () public {
         Assert.equal(tutoriaParaTest.cancelar(), 1, 'Deberia cancelar una tutoria');
         
         Assert.equal(tutoriaParaTest.estaConfirmado(), 0, 'Deberia devolver que no esta confirmada una tutoria');
-
     }
-    
-    
 }
