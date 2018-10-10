8feaf4185dc7d51e3e1be27903db14575302b25c
Facundo <31260330+Facundopeloso@users.noreply.github.com>
2018-10-10 18:42:18 -0300

    Mapping y require faltantes

--- tutoria.sol
+++ tutoria.sol
@@ -1,40 +1,60 @@
 pragma solidity ^0.4.7;
+
 contract Tutoria {
-    
-    
+
+struct  tutoriaData {
 address idProfesor;
 string materia;
 address alumno;
-uint num_confirmar = 1;
-    
-function pedir (string mater, address idProf) public{
-        materia = mater;
-        idProfesor = idProf;
-        alumno = msg.sender;
-        require (alumno != idProf);
+uint num_confirmar;
+uint num_cancelar;
+}
 
+mapping(address => tutoriaData) tutoriasData;
+
+
+function pedir (string mater, address Id_profsor) public{  
+    t.materia = mater;
+    t.idProfesor = Id_profsor;
+    t.alumno = msg.sender;
+    require (msg.sender != Id_profsor);  
+    tutoriaData t = tutoriasData [msg.sender];
+   
     }
+    
+    
+     
 function getMateria() public returns (string) {
-        return materia;
+    return tutoriasData[msg.sender].materia;
     }
     
 function getIdProfesor() public returns (address) {
-        return idProfesor;
-    }
+    return tutoriasData[msg.sender].idProfesor ;
+}
+
 function getAlumno() public returns (address) {
-        return alumno;
-    }
-function confirmar() public returns (uint) {
-        return num_confirmar;
-    }
-function cancelar() public returns (address) {
-        
+    return  tutoriasData[msg.sender].alumno;
+    
     }
     
-function esConfirmado() public returns (address) {
+function confirmar() public returns (uint) {
+    require(tutoriasData[msg.sender].num_confirmar == 0);
+    require(tutoriasData[msg.sender].idProfesor == msg.sender);
+    return tutoriasData[msg.sender].num_confirmar = 0;
         
     }
-function estaConfirmado() public returns (address){
+function cancelar() public returns (uint) { 
+   require (tutoriasData[msg.sender].alumno == msg.sender);
+    require (tutoriasData[msg.sender].num_confirmar == 0);
+    require (tutoriasData[msg.sender].num_cancelar == 1);   
+    return tutoriasData[msg.sender].num_cancelar = 1;
+    }
+    
+function esConfirmado() public returns (uint) { 
+    return tutoriasData[msg.sender].num_confirmar;
+        
+    }
+function estaConfirmado() public returns (address){ //falta esta confirmado
     
     }
     
